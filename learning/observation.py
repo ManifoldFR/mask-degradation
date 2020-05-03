e4b8077f4645683b05f234a7e3748d792b67a428
ManifoldFR <manifoldfr@outlook.com>
2020-05-03 22:05:00 +0200

    update notebook and observation.py

--- learning/observation.py
+++ learning/observation.py
@@ -2,7 +2,7 @@ import torch
 from torch import Tensor
 import pyro
 import pyro.distributions as dist
-from pyro.nn import PyroModule
+from pyro.nn import PyroModule, PyroSample, PyroParam
 
 from scipy import constants
 from physics import penetration
@@ -13,25 +13,19 @@ import configs
 _DEBIT = 85*constants.liter/constants.minute
 
 
-class Observation(PyroModule):
+class ObservationModel(PyroModule):
     """Observation model."""
     
-    def __init__(self, name='', resp_model=configs.respiratorA, debit=_DEBIT):
+    def __init__(self, name=''):
         """
         Parameters
         ----------
         resp_model : callable that generates a tuple (surface_area, layers)
         """
         super().__init__(name=name)
-        
-        self.surface_area_, self.layers_ = resp_model()
-        self.debit = debit
-        self.face_vel = debit / self.surface_area_
-        
-        self.obs_scale = torch.tensor(0.1)
+        self.obs_scale = PyroSample(dist.InverseGamma(torch.tensor(2.), 0.5))
 
-
-    def forward(self, diameters: Tensor):
+    def forward(self, diameters: Tensor, resp_model=configs.respiratorA, debit=_DEBIT):
         """
         Parameters
         ----------
@@ -39,13 +33,15 @@ class Observation(PyroModule):
         resp_model (function):
             Tuple of surface area and list of `~MaskLayers`.
         """
-        
-        
+        surface_area_, layers_ = resp_model()
+
+        face_vel = debit / surface_area_
+
         with pyro.plate("diameters"):
-            phi = penetration.compute_penetration_profile(
-                diameters, self.layers_, self.face_vel,
+            phi = pyro.deterministic("phi", penetration.compute_penetration_profile(
+                diameters, layers_, face_vel,
                 configs.temperature, configs.viscosity, return_log=True
-            )
+            ))
             
             obs = pyro.sample('obs_log', dist.Normal(loc=phi, scale=self.obs_scale))
         return obs
